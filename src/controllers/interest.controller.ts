b8bc5b7da8717c1d9541f3a4eedadccf72bca729
Lordran <dantelopez305@outlook.com>
2023-10-12 03:53:59 -0600

    Adjusted the architecture Spring/Java-like

---
+++ src/controllers/interest.controller.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+const interestController = Router()
+
+
+export default interestController
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+const userController = Router()
+
+
+export default userController
