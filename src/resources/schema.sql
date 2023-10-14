71ac826ab314c2f4f87e83429226e27eac5fd41e
Lordran <dantelopez305@outlook.com>
2023-10-13 23:33:26 -0600

    Placeholder schema for static Interest values

---
+++ src/resources/schema.sql
@@ -0,0 +1,10 @@
+INSERT INTO Interest (name) VALUES ('Espacio');
+INSERT INTO Interest (name) VALUES ('Ciencia');
+INSERT INTO Interest (name) VALUES ('Quimica');
+INSERT INTO Interest (name) VALUES ('Matematicas');
+INSERT INTO Interest (name) VALUES ('Anime');
+INSERT INTO Interest (name) VALUES ('Futbol');
+INSERT INTO Interest (name) VALUES ('Basketbol');
+INSERT INTO Interest (name) VALUES ('Computacion');
+INSERT INTO Interest (name) VALUES ('Programacion');
+INSERT INTO Interest (name) VALUES ('Karate');
