7bace291a1fc64228d4df16236a765b81b18e8ef
Lordran <dantelopez305@outlook.com>
2023-10-24 23:25:41 -0600

    Comented create, put and delete methods , as they are not needed

--- src/controllers/interest.controller.ts
+++ src/controllers/interest.controller.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { createInterest, deleteInterest, getAllInterests, getInterestById, updateInterest } from "services/interest.service";
+import { getAllInterests, getInterestById, } from "services/interest.service";
 
 const interestController = Router()
 
-interestController.post('/new' , createInterest)
+//interestController.post('/new' , createInterest)
 interestController.get('/all' , getAllInterests)
 interestController.get('/:id', getInterestById)
-interestController.put('/update/:id', updateInterest)
-interestController.delete('/delete/:id', deleteInterest)
+//interestController.put('/update/:id', updateInterest)
+//interestController.delete('/delete/:id', deleteInterest)
 
 export default interestController
